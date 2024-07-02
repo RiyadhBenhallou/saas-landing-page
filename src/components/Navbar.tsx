8144a94f4696227afe301ae8a7ebde23e9d766d6
RiyadhBenhallou <riadhriadh2004@gmail.com>
2024-07-02 12:17:27 +0200

    added responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
-import logo from '../assets/logo.svg';
-import { useState } from 'react'
+import logo from "../assets/logo.svg";
+import { useState } from "react";
 
-
-const nav = ['Pricing', 'About', 'Community', 'Contact'];
+const nav = ["Pricing", "About", "Community", "Contact"];
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false); // State for mobile menu visibility
@@ -22,8 +21,16 @@ export default function Navbar() {
           onClick={toggleMenu}
         >
           {/* <img src={hamburgerIcon} alt="Menu" /> */}
-                              
-  
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="40"
+            height="30"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+          </svg>
         </button>
 
         {/* Desktop Navigation */}
@@ -41,8 +48,9 @@ export default function Navbar() {
 
         {/* Mobile Navigation (conditionally rendered) */}
         <div
-          className={`absolute top-full left-0 bg-white w-full md:hidden transition duration-300 ease-in-out ${isOpen ? 'block' : 'hidden'
-            }`}
+          className={`absolute top-full left-0 bg-white w-full md:hidden transition duration-300 ease-in-out ${
+            isOpen ? "block" : "hidden"
+          }`}
         >
           <ul className="space-y-2 px-4 py-6">
             {nav.map((navItem) => (
@@ -50,16 +58,15 @@ export default function Navbar() {
                 <a href="#">{navItem}</a>
               </li>
             ))}
+            <button className="px-6 py-2 bg-brightRed rounded-full text-white baseline border-2 border-brightRed hover:bg-white hover:text-brightRed font-bold">
+              Get Started
+            </button>
           </ul>
         </div>
 
         <button className="hidden md:block px-6 py-2 bg-brightRed rounded-full text-white baseline border-2 border-brightRed hover:bg-white hover:text-brightRed font-bold">
           Get Started
-        </button>                    
-                      
-                      
-                      
-  
+        </button>
       </div>
     </nav>
   );
